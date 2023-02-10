e672273bf38fd478508161585b699f3840222f10
julian <87904137+jans027@users.noreply.github.com>
2023-02-10 17:54:10 -0500

    dando estilos al formulario

--- src/components/sendMail.jsx
+++ src/components/sendMail.jsx
@@ -5,7 +5,8 @@ const App = () => {
     // dinamic class name
     const [classAlert, setClassAlert] = useState('form_input')
     // dinamic placeholder
-    const [placeholderText, setlPaceholderText] = useState('Ingresa tu Nombre')
+    const [placeholderText, setPlaceholderText] = useState(true)
+    // console.log(placeholderText)
 
 
 
@@ -16,8 +17,8 @@ const App = () => {
         certificate_number: "",
     })
 
-    const handleChange = ({ target: {value, name}}) =>
-        setDataForm({...dataForm, [name]: value})
+    const handleChange = ({ target: { value, name } }) =>
+        setDataForm({ ...dataForm, [name]: value })
 
     const emailjs = "prueba"
     const form = useRef();
@@ -42,71 +43,103 @@ const App = () => {
             dataForm.user_name.trim() === "" ||
             dataForm.user_name === null ||
             dataForm.user_name.length === 0 ||
-            isValidName !== true 
-        ){
-            console.log("nombre incorrecto")
+            isValidName !== true
+        ) {
             setClassAlert('class_1');
-            setlPaceholderText('datos incorrectos')
-        }else if (
+            setPlaceholderText(false)
+        } else if (
             dataForm.user_email.trim() === "" ||
             dataForm.user_email === null ||
             dataForm.user_email.length === 0 ||
-            isValidEmail !== true 
-        ){
-            console.log("email incorrecto")
-        }else if(
+            isValidEmail !== true
+        ) {
+            setClassAlert('class_1');
+
+        } else if (
             dataForm.user_phone.trim() === "" ||
             dataForm.user_phone === null ||
             dataForm.user_phone.length === 0 ||
-            isValidphone !== true 
-        ){
-            console.log("phone incorrecto")
-        }else if(
+            isValidphone !== true
+        ) {
+            setClassAlert('class_1');
+
+        } else if (
             dataForm.certificate_number.trim() === "" ||
             dataForm.certificate_number === null ||
             dataForm.certificate_number.length === 0 ||
-            isValidphone !== true 
-        ){
-            console.log("phone incorrecto")
-        }else{
-            
+            isValidphone !== true
+        ) {
+            setClassAlert('class_1');
+
+        } else {
+            setClassAlert('form_input');
             console.log(dataForm)
-            emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, form.current, process.env.REACT_APP_PUBLIC_KEY)
-            .then((result) => {
-                alert('message sent successfully...');
-                console.log(result.text);
-            },
-                (error) => {
-                    console.log(error.text);
-                });
+            emailjs.sendForm(
+                process.env.REACT_APP_SERVICE_ID,
+                process.env.REACT_APP_TEMPLATE_ID,
+                form.current,
+                process.env.REACT_APP_PUBLIC_KEY
+            )
+                .then((result) => {
+                    alert('message sent successfully...');
+                    console.log(result.text);
+                },
+                    (error) => {
+                        console.log(error.text);
+                    });
         }
 
 
-        
+
     };
 
 
     return (
         <div className='cont_form_mail'>
             <form ref={form} onSubmit={sendEmail}>
-                
+
                 <label className='form_label'>Nombre</label>
-                <input onChange={handleChange} className={classAlert} type="text" placeholder={placeholderText} name="user_name" />
+                <input
+                    onChange={handleChange}
+                    className={classAlert}
+                    type="text"
+                    placeholder={placeholderText === true ? 'Ingresa tu Nombre' : 'Datos incorrectos'}
+                    name="user_name"
+                />
 
                 <label className='form_label'>Correo</label>
-                <input onChange={handleChange} className='form_input' type="email" placeholder='Ingresa un correo valido' name="user_email"  />
+                <input
+                    onChange={handleChange}
+                    className={classAlert}
+                    type="email"
+                    placeholder={placeholderText === true ? 'Ingresa un correo valido' : 'Datos incorrectos'}
+                    name="user_email" />
 
                 <label className='form_label'>Telefono</label>
-                <input onChange={handleChange} className='form_input' type="tel" placeholder='Ingresa tu telefono' name="user_phone"  />
+                <input
+                    onChange={handleChange}
+                    className={classAlert}
+                    type="tel"
+                    placeholder={placeholderText === true ? 'Ingresa tu telefono' : 'Datos incorrectos'}
+                    name="user_phone" />
 
                 <label className='form_label'>Numero del Certificado</label>
-                <input onChange={handleChange} className='form_input' type="tel" placeholder='Ingresa el numero del certificado' name="certificate_number"  />
+                <input
+                    onChange={handleChange}
+                    className={classAlert}
+                    type="tel"
+                    placeholder={placeholderText === true ? 'Ingresa el numero del certificado' : 'Datos incorrectos'}
+                    name="certificate_number" />
 
-                <input className='form_btn' type="submit" value="Enviar" />
+                <input
+                    className='form_btn'
+                    type="submit"
+                    value="Enviar" />
             </form>
         </div>
     );
 }
 export default App;
 
-{/* <div className={`class_0 ${condition_1 ? 'class_1' : 'class_2'}${condition_2 ? 'class_3' : ''}`} /> // => "class_0 class_1" */}
+
+
