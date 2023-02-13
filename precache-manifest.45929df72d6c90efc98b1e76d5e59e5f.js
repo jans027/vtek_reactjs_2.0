67525dec0754f6e81f7540b261cff819bfad97a4
julian <87904137+jans027@users.noreply.github.com>
2023-02-13 09:59:07 -0500

    Updates

---
+++ precache-manifest.45929df72d6c90efc98b1e76d5e59e5f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7742df233b0ed5cfdc0",
+    "url": "/vtek_reactjs_2.0/static/css/main.eb42fb74.chunk.css"
+  },
+  {
+    "revision": "b7742df233b0ed5cfdc0",
+    "url": "/vtek_reactjs_2.0/static/js/main.b7742df2.chunk.js"
+  },
+  {
+    "revision": "f87585fc3cf030a93673",
+    "url": "/vtek_reactjs_2.0/static/css/1.19bf96f8.chunk.css"
+  },
+  {
+    "revision": "f87585fc3cf030a93673",
+    "url": "/vtek_reactjs_2.0/static/js/1.f87585fc.chunk.js"
+  },
+  {
+    "revision": "d3c52bc6287af308d78a",
+    "url": "/vtek_reactjs_2.0/static/js/runtime~main.d3c52bc6.js"
+  },
+  {
+    "revision": "fe897dfad900951eb422bbb4480aeee0",
+    "url": "/vtek_reactjs_2.0/static/media/GV_MC_01_V7_Términos_y_condiciones.fe897dfa.pdf"
+  },
+  {
+    "revision": "bb6acc45c7677ad8d81053e71d56ef06",
+    "url": "/vtek_reactjs_2.0/static/media/GV_FC_06_V1_Quejas_y_apelaciones.bb6acc45.docx"
+  },
+  {
+    "revision": "25ae1e345ba9ce9d027f2761330ace1f",
+    "url": "/vtek_reactjs_2.0/static/media/GV_PC_03_V2_Quejas_y_apelaciones.25ae1e34.pdf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/vtek_reactjs_2.0/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/vtek_reactjs_2.0/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/vtek_reactjs_2.0/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/vtek_reactjs_2.0/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "32dd9f90c21708cbe44aa9d4a3616ce8",
+    "url": "/vtek_reactjs_2.0/index.html"
+  }
+];
