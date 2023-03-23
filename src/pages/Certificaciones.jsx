804977faa6b58e7a6c945d6e21dc26e2f6f7550b
julian <87904137+jans027@users.noreply.github.com>
2023-03-23 18:20:05 -0500

    organizando codigo

--- src/pages/Certificaciones.jsx
+++ src/pages/Certificaciones.jsx
@@ -10,25 +10,25 @@ function Certificaciones  ({match})  {
     // console.log(match.params.id)
 
     const data = JSON.parse(localStorage.getItem("element", "value"))
-    console.log(data)
+    const {id, banner1} = data
+    console.log(id, banner1)
 
     // desestructuracion datos de session storage
     const miniaturas = data.miniaturas
     const img = Object.values(miniaturas);
 
 
-
     return (
         <>
             <div className='contCertificaciones'>
                 <div className='BannerPages1'>
-                    <img key={data.id} src={`./images/${data.banner1}`} alt={data.banner1} />
+                    <img key={id} src={require(`../assets/${banner1}`)} alt={banner1} />
                 </div>
                 <div className='BannerPages2'>
-                    <img key={data.id} src={`./images/${data.banner2}`} alt={data.banner1} />
+                    <img key={data.id} src={`../images/${data.banner2}`} alt={data.banner1} />
                 </div>
                 <div className='BannerPages3'>
-                    <img key={data.id} src={`./images/${data.banner3}`} alt={data.banner1} />
+                    <img key={data.id} src={`../images/${data.banner3}`} alt={data.banner1} />
                 </div>
                 <LayoutCotizacion />
                 <div className='containerTextos'>
@@ -43,7 +43,7 @@ function Certificaciones  ({match})  {
                         img.map((item) =>
                             <div className='containerCard' key={item.id}>
                                 <div className='containerImg'>
-                                    <img src={`./images/${item.img}`} alt={item.img} />
+                                    <img src={`../images/${item.img}`} alt={item.img} />
                                 </div>
                                 <p>{item.text}</p>
                             </div>
